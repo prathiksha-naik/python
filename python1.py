fb42865b7c87ec7a594f68b6ec61e43f81dd1124
prathiksha07416 <75781671+prathiksha07416@users.noreply.github.com>
2021-05-11 18:17:49 +0530

    Delete python1.py

--- python1.py
+++
@@ -1,5 +0,0 @@
-flights=100
-takeoffs=int(input("enter the number of takeoffs from airport"))
-landings=int(input("enter the number of landing from airport"))
-current=(flights+landings)-takeoffs
-print("number of flight currently in airport",current)
